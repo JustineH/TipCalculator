c20c5a4079c0a8df13cd07d6a7d21af33a668bda
Justine <justine.m.h@gmail.com>
2016-11-11 11:28:15 -0800

    Initial commit

---
+++ TipCalculator/TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Justine Herman on 11/11/16.
+//  Copyright © 2016 Justine Herman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
