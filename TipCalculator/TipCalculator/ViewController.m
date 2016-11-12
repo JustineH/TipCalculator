bf13b19afbad48dde6997187f7cededfdc242e5b
Justine <justine.m.h@gmail.com>
2016-11-11 18:53:55 -0800

    Add tip percentage, tip amount and total calculations

--- TipCalculator/TipCalculator/ViewController.m
+++ TipCalculator/TipCalculator/ViewController.m
@@ -10,20 +10,59 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (weak, nonatomic) IBOutlet UILabel *totalAmountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+
+@property (nonatomic, assign) double billTotal;
+@property (nonatomic, assign) double tipPercentage;
+@property (nonatomic, assign) double tipAmount;
+@property (nonatomic, assign) double totalAmount;
+
+
 @end
+
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self updateLabels];
+
 }
 
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
+    
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (void)updateLabels
+{
+    double billTotal = self.billAmountTextField.text.doubleValue;
+    
+    float tipPercentage = self.tipPercentageTextField.text.intValue;
+    
+    double tipAmount = (billTotal * tipPercentage)/100;
+    
+    self.totalAmount = billTotal + tipAmount;
+
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"$%.2f", self.tipAmount];
+    self.totalAmountLabel.text = [NSString stringWithFormat:@"$%.2f", self.totalAmount];
+}
+
+- (IBAction)calculateTip:(UIButton *)sender
+{
+    [self updateLabels];
+
+}
+
 
 
 @end
